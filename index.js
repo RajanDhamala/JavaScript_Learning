dded5f3fb8c7e3e0dff945b14b1ea834d7b1b8d6
Rajan Dhamala <157113220+RajanDhamala@users.noreply.github.com>
2024-07-03 21:23:07 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -465,3 +465,84 @@
 //         console.log(key,":-",value)
 // }
 
+        // forin loop
+
+// const hellll={
+//         c:"printf",
+//         java:"System.out.println",
+//         python:"print",
+//         bash:"echo"
+// }
+// for (const key in hellll) {
+//       console.log(`${key} :- ${hellll[key]}`)
+// }
+// const programming=["js","java","python","kotlin","bash"]
+
+// for (const key in programming) {
+//      console.log(`${programming[key]} `)
+// }
+
+        // foreachloop
+//  let programming=["js","java","python","kotlin","bash"]
+//  programming.forEach(function (val){
+//          console.log(val)
+//  })
+
+//         // arrowfunction
+// let programming1=["js","java","python","kotlin","bash"]
+//  programming1.forEach( (hello) => {
+//          console.log(hello)
+//  })
+
+// const arry=[
+//         {language:"English",
+//         speakerdensity:"high"
+//         },
+//         {language:"Hindi",
+//         speakerdensity:"medium"
+//         },
+//         {language:"Nepali",
+//         speakerdensity:"low"
+//         }
+// ]
+// arry.forEach((items,index) =>{
+//         console.log(items.language,items.speakerdensity, index )
+// })
+
+        // filter to return the value in form of array foreach loop cannot return
+// const num1=[1,2,3,4,5,6,7,8,9,10]
+// const result=num1.filter( (item) => item>5)
+
+// const result=num1.filter( function (item,index){
+//         console.log(`${item} and its index is ${index}`)
+//         return item>5
+// })
+// console.log(result)
+
+// const num1=[1,2,3,4,5,6,7,8,9,10]
+// const newnums=[]
+// num1.forEach( (item) => {
+//         if (item>5){
+//                 newnums.push(item)
+//         }
+// }  )
+// console.log(newnums)
+
+// const books = [
+//         { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//         { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//         { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//         { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//         { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//         { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//         { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//         { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//         { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+//       ];
+// const userchoice=books.filter( (item) => item.genre=="History")   
+// console.log(userchoice)
+
+// const userchoice1=books.filter( (item)=>{
+//         return item.publish>=1995 && item.genre =="Science"
+// })
+// console.log(userchoice1)
